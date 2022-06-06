1f87775a62fb091ac6f61119f0daefc0b39ccf55
David Alexander <dalexander03@student.ciputra.ac.id>
2022-06-06 12:31:45 +0700

    Code cleanup in NewsAPI; removed older fetch code

--- ShireNews/Models/NewsAPI.swift
+++ ShireNews/Models/NewsAPI.swift
@@ -46,33 +46,6 @@ struct NewsAPI{
             }
     }
     
-    //    func fetch(from category: Category) async throws -> [Article] {
-    //        let url = generateURL(from: category)
-    //
-    //        let (data, response) = try await session.data(from: url)
-    //
-    //        guard let response = response as? HTTPURLResponse else{
-    //            throw generateError(description: "Bad Response")
-    //        }
-    //
-    //        switch response.statusCode{
-    //            case (200...299), (400...499):
-    //                let apiResponse = try jsonDecoder.decode(APIResponse.self, from:data)
-    //
-    //            if apiResponse.status == "ok" {
-    //                return apiResponse.articles ?? []
-    //            }else{
-    //                throw generateError(description: apiResponse.message ?? "Error")
-    //            }
-    //
-    //            default:
-    //                throw generateError(description: "Error")
-    //
-    //            }
-    //
-    //
-    //    }
-    
     private func generateError(code: Int = 1, description: String) -> Error{
         NSError(domain: "NewsAPI", code: code, userInfo: [NSLocalizedDescriptionKey: description])
     }
