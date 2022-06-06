cae6feff510dae6cbf97b1fc8ca176f044d1a282
David Alexander <dalexander03@student.ciputra.ac.id>
2022-06-06 12:30:19 +0700

    Code cleanup in Article view model; history is never used

--- ShireNews/Viewmodel/ArticleViewModel.swift
+++ ShireNews/Viewmodel/ArticleViewModel.swift
@@ -23,9 +23,6 @@ class ArticleViewModel: ObservableObject{
     
     /// For search function
     @Published var searchQuery = ""
-    @Published var history = [String]()
-    private let historyStore = PlistDataStore<[String]>(filename: "histories")
-    private let historyMaxLimit = 10
     private var trimmedSearchQuery: String{
         searchQuery.trimmingCharacters(in: .whitespacesAndNewlines)
     }
@@ -89,16 +86,6 @@ class ArticleViewModel: ObservableObject{
             phase = .failure(error)
         }
         
-    }
-    
-    func addHistory(_ text: String){
-        if let index = history.firstIndex(where: { text.lowercased() == $0.lowercased() }){
-            history.remove(at: index)
-        }else if history.count == historyMaxLimit{
-            history.remove(at: history.count-1)
-        }
-        
-        history.insert(text, at:0)
     }
     
 }
